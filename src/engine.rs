a077c13efa8f4d1b0bc0fc9c92ae9f031662be03
David <dave.eyal@gmail.com>
2026-09-06 11:51:48 +0200

    Re-enable pawnhist
    Bench: 2643762

--- src/engine.rs
+++ src/engine.rs
@@ -589,7 +589,7 @@ impl Engine {
 
     fn generate_moves(&self, board: &Board, tt_move: Option<Move>) -> MoveList {
         let mut moves = MoveList::new(false);
-        // let pawn_hash = board.pawn_hash(board.side_to_move()) ^ board.pawn_hash(!board.side_to_move());
+        let pawn_hash = board.pawn_hash(board.side_to_move()) ^ board.pawn_hash(!board.side_to_move());
 
         board.generate_moves(|moves_for_piece| {
             for mv in moves_for_piece {
@@ -600,7 +600,7 @@ impl Engine {
                     emv.history = self.capture_history.get(board.side_to_move() as usize, mv.to, emv.piece_type, emv.target_type.unwrap());
                 } else {
                     let quiet_history = self.quiet_history.get(board.side_to_move() as usize, mv.from, mv.to);
-                    // let pawn_history = self.pawn_history.get(board.side_to_move() as usize, pawn_hash, mv.to, emv.piece_type);
+                    let pawn_history = self.pawn_history.get(board.side_to_move() as usize, pawn_hash, mv.to, emv.piece_type);
                     // let mut continuation_history = 0;
 
                     // for ply in 0..min(self.move_stack.len(), CONTHIST_PLY) {
@@ -610,7 +610,7 @@ impl Engine {
                     //     );
                     // }
                     
-                    emv.history = quiet_history; // + pawn_history + continuation_history;
+                    emv.history = quiet_history + pawn_history; // + continuation_history;
                 }
 
                 if emv.is_capture || emv.is_tt {
@@ -1018,15 +1018,15 @@ impl Engine {
             if x >= bounds.beta {
                 // actual = NodeType::CUT;
                 if !mv.is_capture && !mv.promotion {
-                    // let pawn_hash = board.pawn_hash(board.side_to_move()) ^ board.pawn_hash(!board.side_to_move());
+                    let pawn_hash = board.pawn_hash(board.side_to_move()) ^ board.pawn_hash(!board.side_to_move());
                     
                     self.quiet_history.update(stm_index, mv.mv.from, mv.mv.to, history_bonus(depth));
-                    // self.pawn_history.update(stm_index, pawn_hash, mv.mv.to, mv.piece_type, history_bonus(depth));
+                    self.pawn_history.update(stm_index, pawn_hash, mv.mv.to, mv.piece_type, history_bonus(depth));
 
                     for mv2 in picker.tried_quiets() {
                         if mv2.mv != mv.mv {
                             self.quiet_history.update(stm_index,mv2.mv.from, mv2.mv.to, -history_bonus(depth));
-                            // self.pawn_history.update(stm_index, pawn_hash, mv2.mv.to, mv2.piece_type, -history_bonus(depth));
+                            self.pawn_history.update(stm_index, pawn_hash, mv2.mv.to, mv2.piece_type, -history_bonus(depth));
                         }
                     }
 
